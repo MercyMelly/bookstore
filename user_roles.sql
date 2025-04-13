db3c49c49a7d67082e62e17cbf9a2c7137fa6fcd
MercyMelly <mellymercy047@gmail.com>
2025-04-13 16:37:14 +0300

    user roles and permissions

--- user_roles.sql
+++ user_roles.sql
@@ -1,40 +1,63 @@
 
--- role for Mercy admin role - full acces to the database 
-CREATE USER 'mercy_admin'@'localhost' 
+--  admin role..mercy
+CREATE ROLE 'admin_role', 'manager_role', 'staff_role', 'readonly_role';
+
+GRANT ALL PRIVILEGES ON bookstore.* TO 'admin_role';
+
+--manager role..nana
+GRANT INSERT, UPDATE, DELETE ON bookstore.book TO 'manager_role';
+GRANT INSERT, UPDATE, DELETE ON bookstore.publisher TO 'manager_role';
+GRANT INSERT, UPDATE, DELETE ON bookstore.author TO 'manager_role';
+GRANT INSERT, UPDATE, DELETE ON bookstore.book_author TO 'manager_role';
+GRANT INSERT, UPDATE, DELETE ON bookstore.book_language TO 'manager_role';
+
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON bookstore.* TO 'manager_role';
+REVOKE INSERT, UPDATE, DELETE ON bookstore.country FROM 'manager_role';
+REVOKE INSERT, UPDATE, DELETE ON bookstore.book_language FROM 'manager_role';
+REVOKE INSERT, UPDATE, DELETE ON bookstore.address_status FROM 'manager_role';
+REVOKE INSERT, UPDATE, DELETE ON bookstore.order_status FROM 'manager_role';
+
+--  staff role ..promise
+GRANT SELECT ON bookstore.* TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.book TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.book_author TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.cust_order TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.order_line TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.order_history TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.customer TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.customer_address TO 'staff_role';
+GRANT INSERT, UPDATE ON bookstore.address TO 'staff_role';
+
+-- readonly role
+GRANT SELECT ON bookstore.* TO 'readonly_role';
+
+--  users
+CREATE USER 'mercy_admin'@'localhost'
 IDENTIFIED BY '1234';
-
-GRANT ALL ON bookstore.* 
-TO 'mercy_admin'@'localhost';
-
--- role for Nana - inventory management role - inventory management
 CREATE USER 'nana_manager'@'localhost' 
 IDENTIFIED BY '1234';
-
-GRANT SELECT ON bookstore.* 
-TO 'nana_manager'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.book TO 'nana_manager'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.publisher TO 'nana_manager'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.author TO 'nana_manager'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.book_author TO 'nana_manager'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.book_language TO 'nana_manager'@'localhost';
-
--- role for Promise sales and customer management - order processing
-CREATE USER 'promise_sales'@'localhost' 
+CREATE USER 'promise_staff'@'localhost'
+IDENTIFIED BY '1234';
+CREATE USER 'bookstore_readonly'@'localhost' 
 IDENTIFIED BY '1234';
 
-GRANT SELECT ON bookstore.* 
-TO 'promise_sales'@'localhost';
+-- grant roles to users
+GRANT 'admin_role' TO 'mercy_admin'@'localhost';
+GRANT 'manager_role' TO 'nana_manager'@'localhost';
+GRANT 'staff_role' TO 'promise_staff'@'localhost';
+GRANT 'readonly_role' TO 'bookstore_readonly'@'localhost';
 
--- for customer management
-GRANT INSERT, UPDATE, DELETE ON bookstore.customer TO 'promise_sales'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.customer_address TO 'promise_sales'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.address TO 'promise_sales'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.address_status TO 'promise_sales'@'localhost';
 
--- for order processing
-GRANT INSERT, UPDATE, DELETE ON bookstore.cust_order TO 'promise_sales'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.order_line TO 'promise_sales'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.order_history TO 'promise_sales'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.order_status TO 'promise_sales'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON bookstore.shipping_method TO 'promise_sales'@'localhost';
+-- this is to show the created roles and permissions 
+SHOW GRANTS FOR 'admin_role';
+SHOW GRANTS FOR 'manager_role';
+SHOW GRANTS FOR 'staff_role';
+SHOW GRANTS FOR 'readonly_role';
+
+-- this is to show user permissions
+SHOW GRANTS FOR 'mercy_admin'@'localhost';
+SHOW GRANTS FOR 'nana_manager'@'localhost';
+SHOW GRANTS FOR 'promise_staff'@'localhost';
+SHOW GRANTS FOR 'bookstore_readonly'@'localhost';
 
